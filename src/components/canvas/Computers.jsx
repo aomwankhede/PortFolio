185186b1f4b5f295119154601a48c3047ab4347a
aomwankhede <wankhede.aom@gmail.com>
2023-09-12 00:10:41 +0530

    Experience and skills added

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,25 +1,49 @@
 import React, { useEffect, useState, Suspense } from "react";
-import {Canvas } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ ismobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor={"black"}/>
+      <hemisphereLight intensity={0.15} groundColor={"black"} />
       <pointLight intensity={1} />
-      <spotLight position={[-20,50,10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024}/>
-      <primitive object={computer.scene} 
-      scale={0.75} 
-      position={[0,-3.25,-1.5]}
-      rotation={[-0.01,-0.2,-0.1]}
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={ismobile ? 0.7 : 0.75}
+        position={ismobile ? [0, -3, -2, 2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
-  );  
+  );
 };
-  
+
 const ComputersCanvas = () => {
+  const [ismobile, setIsmobile] = useState(false);
+
+  useEffect(() => {
+    const m = window.matchMedia("(max-width:500)");
+    setIsmobile(m.matches);
+
+    const handleChange = (e) => {
+      setIsmobile(e.matches);
+    };
+
+    m.addEventListener("change", handleChange);
+
+    return () => {
+      m.removeEventListener("change", handleChange);
+    };
+  }, []);
   return (
     <Canvas
       frameloop="demand"
@@ -28,11 +52,15 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2} />
-        <Computers/>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers ismobile={ismobile} />
       </Suspense>
-      <Preload all/>
-    </Canvas> 
+      <Preload all />
+    </Canvas>
   );
 };
 
